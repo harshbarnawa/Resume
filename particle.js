7c18b5e3423eb4b4c0d09c6adecd76e4d922f4b4
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-03-06 16:45:10 +0530

    added particle background

---
+++ particle.js
@@ -0,0 +1,22 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    size: { value: 3 },
+    color: { value: "#38bdf8" },
+    line_linked: {
+      enable: true,
+      color: "#38bdf8"
+    },
+    move: {
+      speed: 2
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  }
+});
